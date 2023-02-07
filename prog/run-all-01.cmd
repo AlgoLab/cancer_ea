aadbc08de19abfb6ce8c533b51afdff34c07f94b
Vladimir Filipovic <vladofilipovic@hotmail.com>
2023-02-07 08:58:49 +0100

    .cmd improved

--- prog/run-all-01.cmd
+++ prog/run-all-01.cmd
@@ -28,3 +28,5 @@ REM
 python ./src/genetic_programming_dollo/main_random_init.py
 REM 
 python ./src/genetic_programming_dollo/main_gp_direct.py
+REM 
+python ./src/genetic_programming_dollo/main_gp_infer.py
